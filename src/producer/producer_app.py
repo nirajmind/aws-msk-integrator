58684ab377dbe5aba2c7e4f80861243b3edce956
Niraj Adhikary <nirajadhikary@yahoo.com>
2026-05-04 18:06:42 +0530

    Rewrote producer app also

--- src/producer/producer_app.py
+++ src/producer/producer_app.py
@@ -1,26 +1,17 @@
-from confluent_kafka import Producer
-from src.config.settings import SSL_CONFIG, TOPIC
+from src.producer.producer_service import ProducerService
+from src.config.settings import TOPIC, SSL_CONFIG
 from src.common.logger import get_logger
 
 log = get_logger("producer")
-
-def delivery_report(err, msg):
-    if err:
-        log.error(f"Delivery failed: {err}")
-    else:
-        log.info(f"Delivered to {msg.topic()} [{msg.partition()}] offset {msg.offset()}")
 
 def main():
     log.info(f"BOOTSTRAP: {SSL_CONFIG['bootstrap.servers']}")
     log.info(f"TOPIC: {TOPIC}")
 
-    producer = Producer(SSL_CONFIG)
+    svc = ProducerService()
 
     for i in range(10):
-        producer.produce(TOPIC, f"msg-{i}".encode("utf-8"), callback=delivery_report)
-        producer.flush()
-
-    log.info("Produced 10 messages")
+        svc.send(f"msg-{i}")
 
 if __name__ == "__main__":
     main()
