8eb5a9a9bffc7d6961399c8bebc14721ce23e541
Niraj Adhikary <nirajadhikary@yahoo.com>
2026-05-04 18:02:02 +0530

    Using proper names for loading the env variables at runtime

--- src/config/settings.py
+++ src/config/settings.py
@@ -2,9 +2,9 @@ from src.common.env_loader import load_env
 
 env = load_env()
 
-BOOTSTRAP = env["bootstrap"]
-TOPIC = env["topic"]
-GROUP_ID = env["group_id"]
+BOOTSTRAP = env["MSK_BOOTSTRAP"]
+TOPIC = env["MSK_TOPIC"]
+GROUP_ID = env["GROUP_ID"]
 
 SSL_CONFIG = {
     "bootstrap.servers": BOOTSTRAP,
